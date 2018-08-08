493bca5a1042c06ca75d4e921a731fc64023510d
Julian Gründner <juliangruendner@googlemail.com>
2018-08-08 15:14:28 +0200

    add subrepos to gitignore and gitupdate.sh

---
+++ gitUpdate.sh
@@ -0,0 +1,19 @@
+githubBase="git@github.com:juliangruendner/"
+mlServiceRepos=("ds_common" "ds_poll" "ds_queue" "datashield_docker")
+baseDir=$(pwd)
+
+echo "****updating base repo Develop****"
+git pull
+
+for repoName in ${mlServiceRepos[@]}
+do
+  curRepo="$baseDir/$repoName"
+  if [ ! -d "$curRepo" ]
+  then
+        cd $baseDir
+        echo "****initialising git repo $repoName****"
+        git clone "$githubBase$repoName.git"
+  fi
+done
+
+ls | grep '\<mlService_' | xargs -P10 -I{} git --git-dir={}/.git --work-tree=$PWD/{} pull origin master
