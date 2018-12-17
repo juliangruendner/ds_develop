ce036194b34b8c0afda96edcf7d7b9bb301bef0c
Julian Gründner <juliangruendner@googlemail.com>
2018-12-17 16:19:44 +0100

    fill install_prod_queue.sh

--- build_from_images/queue/install_prod_queue.sh
+++ build_from_images/queue/install_prod_queue.sh
@@ -3,4 +3,20 @@
 # this scripts installs the queue server for you
 source ./queue.config
 
+if [[ $(which docker) ]]; then
+    echo "docker already installled, version is: "
+    docker -v
+    
+else
+    echo "docker not installed, installing docker:"
+    cd ~/ds_deployment/ds_develop
+    ./install_docker.sh
+fi
 
+
+if [[ -n $QP_DOCKER_REGISTRY_PREFIX ]]; then
+    printf "**** registry fround pulling images from $QP_DOCKER_REGISTRY_PREFIX ...\n\n"
+   ./pullQueueProdImages.sh $QP_DOCKER_REGISTRY_PREFIX $QP_DOCKER_REGISTRY_USER $QP_DOCKER_REGISTRY_PW
+fi
+
+./start_prod.sh
