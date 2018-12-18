0909c71e930aa09f3d941ee1de446683880e3b49
Julian Gründner <juliangruendner@googlemail.com>
2018-12-18 08:17:04 +0100

    add poll and queue config to install packages when generating them

--- build_from_images/create_install_package.sh
+++ build_from_images/create_install_package.sh
@@ -1,2 +1,11 @@
-zip -r opal_poll_install.zip opal_poll/ -x opal_poll/opal_poll.config
-zip -r queue_install.zip queue/ -x queue/queue.config
+cp ../opal_poll.config_sample ./opal_poll.config
+cp ../queue.config_sample ./queue.config
+
+rm opal_poll_install.zip
+rm queue_install.zip
+
+zip -rj opal_poll_install.zip opal_poll.config opal_poll/ -x opal_poll/opal_poll.config 
+zip -rj queue_install.zip queue.config queue/ -x queue/queue.config
+
+rm ./opal_poll.config
+rm ./queue.config
