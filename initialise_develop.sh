b925714521c1f5810a7ae3cd037c6b7860f76aed
Julian Gründner <juliangruendner@googlemail.com>
2019-01-17 12:29:52 +0100

    finalize init development environment

--- initialise_develop.sh
+++ initialise_develop.sh
@@ -1,7 +1,9 @@
-#! bash
 # this initialises the needed base containers for rstudio and the poll monitor server
 
-cd ds_rstudio
+cd ds_opal/opal_with_init
+docker build -f Dockerfile -t opal_docker .
+
+cd ../../ds_rstudio
 docker build -f Dockerfile.rstudio.base -t rstudio_server .
 docker build -f Dockerfile.rstudio.ds -t ds_rstudio_server .
 
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd ds_opal
 ./start.sh
 
-#cd ../ds_poll_monitor_server
+# cd ../ds_poll_monitor_server
 #./start.sh
 
 cd ../ds_rstudio
